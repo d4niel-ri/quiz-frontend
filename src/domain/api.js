40bd435b769205c65b263c60de1e832500928ce5
Daniel Riyanto <danielriyanto190700@gmail.com>
2023-11-21 10:48:44 +0700

    feat(FSB3-23): Initialize repository

---
+++ src/domain/api.js
@@ -0,0 +1,32 @@
+import config from '@config/index';
+import { merge } from 'lodash';
+
+import request from '@utils/request';
+
+const urls = {
+  ping: '',
+  user: '/api/user',
+  quiz: '/api/quiz',
+};
+
+export const callAPI = async (endpoint, method, header = {}, params = {}, data = {}) => {
+  const defaultHeader = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  };
+
+  const headers = merge(defaultHeader, header);
+  const options = {
+    url: config.api.host + endpoint,
+    method,
+    headers,
+    data,
+    params,
+  };
+
+  return request(options).then((response) => {
+    const responseAPI = response.data;
+    return responseAPI;
+  });
+};
+
+export const ping = () => callAPI(urls.ping, 'get');
